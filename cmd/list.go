e62d4cf4342a7367e46fd96335a0c7d541f28ab7
Spencer Archibald <spicyspice.dev@gmail.com>
2025-03-01 18:58:57 -0600

    Add month, year args for which month to view expenses

--- cmd/list.go
+++ cmd/list.go
@@ -1,7 +1,9 @@
 package cmd
 
 import (
-    "fmt"
+	"fmt"
+	"time"
+
 	"github.com/spf13/cobra"
 
 	"budget_buddy/budget/utils"
@@ -13,6 +15,7 @@ var listCmd = &cobra.Command{
     Run: func(cmd *cobra.Command, args []string) {
         user, _ := cmd.Flags().GetString("user")
         userDirectory := fmt.Sprintf("%s/%s", utils.DataFilesDirectory, user)
+        fmt.Println(userDirectory)
         fileName := fmt.Sprintf("%s/%s", userDirectory, utils.GetCurrentMonthFile())
 
         if !utils.DataFileExists(userDirectory) || !utils.DataFileExists(fileName) {
@@ -26,5 +29,18 @@ var listCmd = &cobra.Command{
 }
 
 func init() {
+    currentDate := time.Now()
+    currentMonth := int(currentDate.Month())
+    currentYear := int(currentDate.Year())
+
+    fmt.Println(currentYear)
+
     listCmd.Flags().StringP("user", "u", "", "User associated with expenses")
+    listCmd.Flags().IntP("month", "m", currentMonth, "Which month of expenses to view")
+    listCmd.Flags().IntP("year", "y", currentYear, "Which year of the month the expenses to view")
+
+    listCmd.MarkFlagsRequiredTogether("month", "year")
 }
+
+
+//TODO: Fix output of expense list
